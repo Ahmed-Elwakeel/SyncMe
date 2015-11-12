608101010d4475ea17b2402985261e8d6c3580e6
Ahmed-Elwakeel <ahmed.o.elwakeel@gmail.com>
2015-11-12 22:21:37 +0300

    adding devise

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151112123226) do
+ActiveRecord::Schema.define(version: 20151112183358) do
 
   create_table "friendposts", force: :cascade do |t|
     t.integer  "user_a",     limit: 4
@@ -52,19 +52,29 @@ ActiveRecord::Schema.define(version: 20151112123226) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string   "f_name",      limit: 255
-    t.string   "l_name",      limit: 255
-    t.string   "country",     limit: 255
-    t.string   "city",        limit: 255
-    t.date     "b_date"
-    t.boolean  "gender",      limit: 1
-    t.string   "profile_pic", limit: 255
-    t.text     "bio",         limit: 65535
-    t.string   "email",       limit: 255
-    t.string   "password",    limit: 255
-    t.string   "phone",       limit: 255
-    t.datetime "created_at",                null: false
-    t.datetime "updated_at",                null: false
+    t.string   "name",                   limit: 255, default: "", null: false
+    t.string   "email",                  limit: 255, default: "", null: false
+    t.string   "encrypted_password",     limit: 255, default: "", null: false
+    t.string   "about",                  limit: 255
+    t.string   "avatar",                 limit: 255
+    t.string   "cover",                  limit: 255
+    t.string   "reset_password_token",   limit: 255
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",          limit: 4,   default: 0,  null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string   "current_sign_in_ip",     limit: 255
+    t.string   "last_sign_in_ip",        limit: 255
+    t.string   "confirmation_token",     limit: 255
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.datetime "created_at",                                      null: false
+    t.datetime "updated_at",                                      null: false
   end
+
+  add_index "users", ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true, using: :btree
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
 end
